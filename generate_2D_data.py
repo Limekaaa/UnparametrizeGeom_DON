a862e0211067a30ab3d1ada701c1268e0f9f2745
Limekaaa <maxime.hanus@gmail.com>
2025-07-15 11:43:12 +0200

    overcome memory limitation issue

--- generate_2D_data.py
+++ generate_2D_data.py
@@ -17,6 +17,16 @@ if __name__ == "__main__":
         required=True,
         help="The experiment directory. This directory should include specifications file 'specs.json' and 'specs_data.json'."
     )
+
+    parser.add_argument(
+        "--batch_size",
+        "-b",
+        dest="batch_size",
+        type=int,
+        default=10,
+        help="The batch size for data generation."
+    )
+
     args = parser.parse_args()
 
     #specs_filename = f"{args.experiment_directory}/specs.json"
@@ -40,24 +50,25 @@ if __name__ == "__main__":
         rhs = eval(specs_data["PDEData"]["rhs"])
 
         coeffs = [1.0] if specs_data["PDEData"]["n_coeffs"] == 1 else [random.uniform(0.0, 2.0) for _ in range(specs_data["PDEData"]["n_coeffs"])]
+        batch_coeffs = [coeffs[i : i + args.batch_size] for i in range(0, len(coeffs), args.batch_size)]
+        for coeff in batch_coeffs:
+            data_gen = Poisson2D_random_shape(
+                mesh,
+                coeffs=coeff,
+                rhs=rhs,
+                bc_val=specs_data["PDEData"]["bc_val"],
+            )
+            data = data_gen.create_data()
 
-        data_gen = Poisson2D_random_shape(
-            mesh,
-            coeffs=coeffs,
-            rhs=rhs,
-            bc_val=specs_data["PDEData"]["bc_val"],
-        )
-        data = data_gen.create_data()
-
-        # Save the data to a file
-        for i, (coeff, sol) in enumerate(data):
-            path_to_save = os.path.join(specs_data["root_dir"], specs_data["dataset_name"], "PDEData", msh_filename[:-4])
-            c = 0
-            while os.path.exists(path_to_save):
-                c += 1
-                path_to_save = os.path.join(specs_data["root_dir"], specs_data["dataset_name"], "PDEData", f"{msh_filename[:-4]}_{c}")
-            os.makedirs(path_to_save, exist_ok=True)
-            np.savez(os.path.join(path_to_save, f"coeff_{coeff:.4f}.npz"), rhs=np.array(coeff), sol=sol, coords=coords)
+            # Save the data to a file
+            for i, (coeff, sol) in enumerate(data):
+                path_to_save = os.path.join(specs_data["root_dir"], specs_data["dataset_name"], "PDEData", msh_filename[:-4])
+                c = 0
+                while os.path.exists(path_to_save):
+                    c += 1
+                    path_to_save = os.path.join(specs_data["root_dir"], specs_data["dataset_name"], "PDEData", f"{msh_filename[:-4]}_{c}")
+                os.makedirs(path_to_save, exist_ok=True)
+                np.savez(os.path.join(path_to_save, f"coeff_{coeff:.4f}.npz"), rhs=np.array(coeff), sol=sol, coords=coords)
 
     train_msh_filenames = random.sample(msh_filenames, int(len(msh_filenames) * specs_data["Split"]["train_proportion"]))
     test_msh_filenames = [f for f in msh_filenames if f not in train_msh_filenames]
